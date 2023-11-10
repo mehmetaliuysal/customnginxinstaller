d889852e123eea535b3add5d630d70ca8f269ebc
Mehmet Ali UYSAL <mehmetali@gelistir.org>
2023-11-11 00:01:41 +0300

    Update installer.py

--- installer.py
+++ installer.py
@@ -5,30 +5,14 @@ import time
 # Color codes for ANSI
 class Colors:
     HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
-
-print(Colors.HEADER + "Starting Custom Nginx Installer..." + Colors.ENDC)
-print(Colors.HEADER + "Waiting 10 seconds for abort [CTRL-C]..." + Colors.ENDC)
-time.sleep(10)  # Give the user 10 seconds to abort
-
-
-def run_command(command, success_message, change_dir=False):
+def run_command(command, success_message):
     """ Run a shell command and print a success message if it succeeds """
     try:
-        if change_dir:
-            dir_command, actual_command = command.split(' && ', 1)
-            os.chdir(dir_command.replace('cd ', ''))
-            subprocess.check_call(actual_command, shell=True)
-        else:
-            subprocess.check_call(command, shell=True)
-
+        subprocess.check_call(command, shell=True)
         print(Colors.OKGREEN + success_message + Colors.ENDC)
     except subprocess.CalledProcessError:
         print(Colors.FAIL + f"Error occurred: {command}" + Colors.ENDC)
@@ -85,73 +69,46 @@ WantedBy=multi-user.target
     except Exception as e:
         print(Colors.FAIL + f"Failed to create /etc/systemd/system/nginx.service: {e}" + Colors.ENDC)
 
-
 def is_git_installed():
     """ Check if Git is installed """
     try:
         subprocess.check_call(["git", "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         return True
-    except subprocess.CalledProcessError:
-        return False
-    except FileNotFoundError:
+    except (subprocess.CalledProcessError, FileNotFoundError):
         return False
 
+print(Colors.HEADER + "Starting Custom Nginx Installer..." + Colors.ENDC)
+print(Colors.HEADER + "Waiting 10 seconds for abort [CTRL-C]..." + Colors.ENDC)
+time.sleep(10)  # Give the user 10 seconds to abort
 
-
-
+# Check if Git is installed and install if not
 if not is_git_installed():
     print(Colors.WARNING + "Git is not installed. Installing Git..." + Colors.ENDC)
     run_command("sudo apt-get install git -y", "Git successfully installed.")
 
-# Initial setup commands
-initial_commands = [
-    ("cd /tmp", "Changed directory to /tmp", True),
-    ("sudo apt update", "Package lists updated",True),
-    ("sudo apt-get install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev libgd-dev libxslt1-dev libxml2-dev libgeoip-dev libgoogle-perftools-dev libperl-dev git wget -y", "Required packages installed",True),
-]
+# Update package lists and install necessary packages
+run_command("sudo apt update", "Package lists updated")
+run_command("sudo apt-get install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev libgd-dev libxslt1-dev libxml2-dev libgeoip-dev libgoogle-perftools-dev libperl-dev wget -y", "Required packages installed")
 
-# Running the initial setup commands
-for cmd, message, change_dir in initial_commands:
-    run_command(cmd, message, change_dir)
+# Download, extract, and compile Nginx
+os.chdir('/tmp')
+run_command("wget http://nginx.org/download/nginx-1.24.0.tar.gz", "Downloaded Nginx tarball")
+run_command("tar zxvf nginx-1.24.0.tar.gz", "Extracted Nginx tarball")
+os.chdir('/tmp/nginx-1.24.0')
+run_command("./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/lock/nginx.lock --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --user=nginx --group=nginx --with-threads --with-file-aio --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module=dynamic --with-http_image_filter_module=dynamic --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_auth_request_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_slice_module --with-http_stub_status_module --with-http_perl_module=dynamic --with-pcre --with-pcre-jit --with-stream=dynamic --with-stream_ssl_module --with-mail=dynamic --with-mail_ssl_module --with-google_perftools_module --with-debug --add-dynamic-module=../nginx-module-vts --add-dynamic-module=../nginx-module-sts --add-dynamic-module=../nginx-module-stream-sts", "Nginx configured")
+run_command("make", "Nginx build completed")
+run_command("sudo make install", "Nginx installed")
 
+# Additional setup for Nginx
+run_command("sudo adduser --system --no-create-home --disabled-login --disabled-password --group nginx", "Nginx user created")
+run_command("sudo mkdir -p /var/lib/nginx/body && sudo mkdir -p /var/lib/nginx/proxy && sudo mkdir -p /var/lib/nginx/fastcgi && sudo mkdir -p /var/lib/nginx/uwsgi && sudo mkdir -p /var/lib/nginx/scgi && sudo chown -R nginx:nginx /var/lib/nginx", "Nginx directories created and permissions set")
 
-# Nginx configuration and installation commands
-commands = [
-    ("cd /tmp && wget http://nginx.org/download/nginx-1.24.0.tar.gz && tar zxvf nginx-1.24.0.tar.gz", "Downloaded and extracted Nginx", True),
-    ("cd /tmp/nginx-1.24.0 && ./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/lock/nginx.lock --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --user=nginx --group=nginx --with-threads --with-file-aio --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module=dynamic --with-http_image_filter_module=dynamic --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_auth_request_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_slice_module --with-http_stub_status_module --with-http_perl_module=dynamic --with-pcre --with-pcre-jit --with-stream=dynamic --with-stream_ssl_module --with-mail=dynamic --with-mail_ssl_module --with-google_perftools_module --with-debug --add-dynamic-module=../nginx-module-vts --add-dynamic-module=../nginx-module-sts --add-dynamic-module=../nginx-module-stream-sts", "Nginx configured", True),
-    ("cd /tmp/nginx-1.24.0 && make", "Nginx build completed", True),
-    ("cd /tmp/nginx-1.24.0 && sudo make install", "Nginx installed", True),
-    # Add other commands here...
-]
-
-# Running the commands
-for cmd, message, change_dir in commands:
-    run_command(cmd, message, change_dir)
-
-# Additional setup commands
-setup_commands = [
-    ("sudo adduser --system --no-create-home --disabled-login --disabled-password --group nginx", "Nginx user created",True),
-    ("sudo mkdir -p /var/lib/nginx/body && sudo mkdir -p /var/lib/nginx/proxy && sudo mkdir -p /var/lib/nginx/fastcgi && sudo mkdir -p /var/lib/nginx/uwsgi && sudo mkdir -p /var/lib/nginx/scgi && sudo chown -R nginx:nginx /var/lib/nginx", "Nginx directories created and permissions set", True),
-    # Add the code for updating /etc/nginx/nginx.conf and creating the service file here...
-]
-
-# Running the additional setup commands
-for cmd, message in setup_commands:
-    run_command(cmd, message)
-
-# Update nginx.conf file and create systemd service file here...
 update_nginx_conf()
 create_nginx_service()
-# Note: File manipulation in Python requires careful handling to avoid corrupting the files.
 
-# Final system commands
-final_commands = [
-    ("sudo systemctl daemon-reload", "System daemon reloaded", True),
-    ("sudo systemctl enable nginx", "Nginx service enabled", True),
-]
 
-# Running the final system commands
-for cmd, message in final_commands:
-    run_command(cmd, message)
+# Reload system daemon and enable nginx service
+run_command("sudo systemctl daemon-reload", "System daemon reloaded")
+run_command("sudo systemctl enable nginx", "Nginx service enabled")
 
 print(Colors.HEADER + "Nginx installation and setup completed successfully!" + Colors.ENDC)
